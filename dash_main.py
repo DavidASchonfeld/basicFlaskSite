55a9986e411017bb0d043a1c64ac8119099eee49
David Schonfeld <davidaschonfeld@gmail.com>
2026-01-25 18:44:38 -0500

    Attempt 1 in incorporating Dash into my Flask app

---
+++ dash_main.py
@@ -0,0 +1,9 @@
+import dash
+
+def incorporate_into_server_Dash(in_server_object):
+    dash_app = dash.Dash(
+        __name__,
+        server = in_server_object,
+        url_base_pathname = '/dashSection'
+    )
+    return dash_app
--- flask_main.py
+++ flask_main.py
@@ -1,19 +1,34 @@
+
+# Python Libraries
 from flask import Flask
 
-app = Flask(__name__)
+# My Files
+from dash_main import incorporate_into_server_Dash
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Default: Hello"
+# app = Flask(__name__)
 
-@app.route('/test_helloWorld')
-@app.route('/helloWorld')
-def test_helloWorld():
-    return "Hello, World!"
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     return "Default: Hello"
+
+# @app.route('/test_helloWorld')
+# @app.route('/helloWorld')
+# def test_helloWorld():
+#     return "Hello, World!"
+
+
+
+def setup_FlaskApp():
+    app = Flask(__name__)
+    incorporate_into_server_Dash(app)
+    return app
+
+
 
 # Runs if you call the script directly:
 if __name__ == '__main__':
+    app = setup_FlaskApp()
     app.run(host = '0.0.0.0', port = 9000)
 
 
